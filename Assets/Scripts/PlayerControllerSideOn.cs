746df8f1b1e1bdc7db1e7feddc9f80c69bcb08df
Ashley Klahm <klahmashley@gmail.com>
2026-09-19 17:10:28 -0400

    Additional Changes b4 submission

--- Assets/Scripts/PlayerControllerSideOn.cs
+++ Assets/Scripts/PlayerControllerSideOn.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 
 public class PlayerControllerSideOn : MonoBehaviour
 {
@@ -10,24 +9,11 @@ public class PlayerControllerSideOn : MonoBehaviour
     public float speed = 8f;
     public float jumpingPower = 16f;
     private bool isFacingRight = true;
-    private bool isJumping;
-    private float jumpTimeCounter;
-    public float jumpTime;
-
-   // public Animator animator;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask Floor;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         vertical = Input.GetAxisRaw("Vertical");
@@ -38,40 +24,12 @@ public class PlayerControllerSideOn : MonoBehaviour
             rb.linearVelocity = Vector2.up * jumpingPower;
         }
 
-        if (horizontal != 0f)
-        {
-          //  animator.SetBool("isWalking", true);
-        }
-        if (horizontal == 0f)
-        {
-          //  animator.SetBool("isWalking", false);
-         //   animator.SetBool("isRunning", false);
-         //   animator.SetBool("hasJumped", false);
-        }
-
         Flip();
-
-        if (horizontal != 0f)
-        {
-       //     animator.SetBool("IsWalking", true);
-        }
-
-        if (horizontal == 0f)
-        {
-           // animator.SetBool("IsWalking", false);
-        }
-
     }
 
     private void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
-    }
-
-    private bool IsGrounded()
-    {
-        // Debug.Log("HitGround");
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, Floor);
     }
 
     private void Flip()
